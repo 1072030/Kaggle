0561b7369d042c7ccd7066d4d89199fc1106f7c3
1072030 <229jdu@gmail.com>
2023-03-08 14:40:19 +0800

    update

--- Titanic/main.py
+++ Titanic/main.py
@@ -5,30 +5,25 @@
 
 #Python Libraries
 
-import numpy as np # linear algebra
 import pandas as pd # data processing
 import os
 import seaborn as sns
 import matplotlib.pyplot as plt # data visualization
-from pandas.plotting import scatter_matrix # data visualization
 
-from sklearn.model_selection import train_test_split # Machine Learning - split dataset (train/test)
-from sklearn.model_selection import StratifiedShuffleSplit # Machine Learning - stratified shuffle split (train/test)
-from sklearn.impute import SimpleImputer
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import LabelEncoder
 #讀取data
 training_data = pd.read_csv(os.getcwd()+"/train.csv")
-#,index_col='Survived'
 df_train = pd.read_csv(os.getcwd()+"/train.csv")
 df_test = pd.read_csv(os.getcwd()+"/test.csv")
 total_data = pd.DataFrame()
 total_data = total_data.append(df_train)
 total_data = total_data.append(df_test)
-print(total_data.head())
-# drop(inplace=True) 對原始值進行修改
+print(total_data.head()) # check data
 
-# df_y = pd.read_csv(os.getcwd()+"/train.csv",usecols=["Survived"])
-print(df_train.head())
+print(df_train.head()) # check data
 print(df_train.isnull().sum()) # ckeck null values
 
 
@@ -39,31 +34,34 @@ total_data.loc[df_train["Age"]<=16,"Age"]=1
 total_data.loc[df_train["Age"]!=1,"Age"]=2
 print("Age value",total_data["Age"].unique()) #確認值
 sns.barplot(data=df_train,x="Embarked",y="Survived")
-# plt.show()
-total_data["Embarked"].fillna("S",inplace=True)
+plt.show()
 
 # clear data
 # Cabin 缺失值太多 PassengerId,Ticket,Fare 跟存活率較無關
 total_data.drop(["Cabin","PassengerId","Ticket","Fare","Name"],axis=1,inplace=True)
+total_data["Embarked"].fillna("S",inplace=True)
 lable_1 = LabelEncoder()
 total_data["Embarked"] = lable_1.fit_transform(total_data["Embarked"])
+total_data.loc[total_data["Sex"]=="male","Sex"] = 1
+total_data.loc[total_data["Sex"]!=1,"Sex"] = 2
+
 
 # 處理 SibSp(兄弟姊妹)
 sns.countplot(df_train,x=df_train["SibSp"]) #show data
 #　0占大多數 代表大多數沒有兄弟姊妹
-# plt.show()
+plt.show()
 #比對 SibSp 特徵存活率 1和2生存率較高分為一組
 sns.barplot(data=df_train,x="SibSp",y="Survived") #show data
-# plt.show()
+plt.show()
 
 total_data.loc[(total_data["SibSp"]==1) | (total_data["SibSp"]==2),"SibSp"]=1
 total_data.loc[total_data["SibSp"]>2,"SibSp"] = 2
 total_data.loc[total_data["SibSp"]==0,"SibSp"] = 0
-print(total_data["SibSp"].value_counts())
+print(f"""SibSp values:\n{total_data["SibSp"].value_counts()}""")
 
 # 處理 Parch(父母或孩子的數量) 
 sns.countplot(df_train,x=df_train["Parch"]) #show data
-# plt.show() # 0 占多數 1和2少量
+plt.show() # 0 占多數 1和2少量
 
 sns.barplot(data=df_train,x="Parch",y="Survived") #show data
 plt.show()
@@ -71,10 +69,40 @@ plt.show()
 total_data.loc[(total_data["Parch"]==1) | (total_data["Parch"]==2),"Parch"]=1
 total_data.loc[total_data["Parch"]>2,"Parch"] = 2
 total_data.loc[total_data["Parch"]==0,"Parch"] = 0
-print(total_data["Parch"].value_counts())
+print(f"""Parch values:\n{total_data["Parch"].value_counts()}""")
 
 # test_data有418 所以Survived is null 有418 由此可知空值都處理完畢
-print(total_data.isnull().sum())
+print(total_data.head()) #check data
+print(total_data.isnull().sum()) # check null values
+print("-------------------------------------")
+# 製作train set and test set
+x = []
+y = "Survived"
+for j in total_data.keys():
+    if j != y:
+        x.append(j)
+df_x = total_data[total_data[y].notnull()][x].values
+df_y = total_data[total_data[y].notnull()][y].values
+test_x = total_data[total_data[y].isnull()][x].values
+# 確認大小
+print(f"shape:[{df_x.shape},{df_y.shape},{test_x.shape}]")
+
+# 分類
+X_train, X_test, Y_train, Y_test = train_test_split(df_x,df_y,test_size=0.2,random_state=60,shuffle=True)
+print(f"測試大小:[{X_train.shape},{Y_train.shape},{X_test.shape},{Y_test.shape}]")
+# classifier
+classifier = MLPClassifier(activation='relu',solver='adam',alpha=0.0001)
+classifier.fit(X_train,Y_train)
+print("訓練分數",classifier.score(X_train,Y_train))#訓練分數
+Y_pred = classifier.predict(X_test)
+print("測試分數",accuracy_score(Y_test,Y_pred))#測試分數
+
+final_pred = classifier.predict(test_x)
+# 轉成整數格式
+final_pred = final_pred.astype(int)
+
+final_set = pd.DataFrame({"PassengerId":df_test["PassengerId"],"Survived":final_pred})
+final_set.to_csv("submission.csv",index=False)
 
 
 
